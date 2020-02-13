17a8eedb80393f36b93d8c55fd98650f86fcbfb9
Loc Ngo <locqngo@gmail.com>
2020-02-13 00:33:53 -0600

    works with different filing status

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <vector>
-#include <string>
 using namespace std;
 
 /*    ACTUAL TAX BRACKET FISCAL 2019 (DUE 2020)
@@ -22,32 +20,30 @@ void taxtext()
     return;
 }
 
+void taxcalculation(double, double, double, double, double, double, double);
+
 class Bracket {
 public:
-    //vector<double> b(5);
     double b1, b2, b3, b4, b5, b6;
 };
 
 int main()
-{
-    //tax rates
-    double r1 = 0.10, r2 = 0.12, r3 = 0.22, r4 = 0.24, r5 = 0.32, r6 = 0.35, r7 = 0.37;
+{   //variables
+    int status;
+    double b1, b2, b3, b4, b5, b6;
+    double inc; //= 0; 
 
+    //tax brackets
     Bracket sin;
     sin.b1 = 9700, sin.b2 = 39475, sin.b3 = 84200, sin.b4 = 160725, sin.b5 = 204100, sin.b6 = 510300;
-
     Bracket mfj;
     mfj.b1 = 19400, mfj.b2 = 78950, mfj.b3 = 168400, mfj.b4 = 321450, mfj.b5 = 408200, mfj.b6 = 612350;
-
     Bracket mfs;
     mfs.b1 = 9700, mfs.b2 = 39475, mfs.b3 = 84200, mfs.b4 = 160725, mfs.b5 = 204100, mfs.b6 = 306750;
-
     Bracket hoh;
     hoh.b1 = 13850, hoh.b2 = 52850, hoh.b3 = 84200, hoh.b4 = 160700, hoh.b5 = 204100, hoh.b6 = 510300;
- 
-    int status;
-    double b1, b2, b3, b4, b5, b6, inc, tax, taxpercent;
-
+    
+    //ask for input
     cout << "enter filing status:" << endl;
     cout << "enter [1] for single" << endl;
     cout << "enter [2] for married filing separately" << endl;
@@ -58,60 +54,69 @@ int main()
     cout << "enter income" << endl;
     cin >> inc;
 
-
-
+    //find filing status
     switch (status) {
     case 1:
         b1 = sin.b1, b2 = sin.b2, b3 = sin.b3, b4 = sin.b4, b5 = sin.b5, b6 = sin.b6;
+        taxcalculation(inc, b1, b2, b3, b4, b5, b6);
         break;
     case 2:
         b1 = mfj.b1, b2 = mfj.b2, b3 = mfj.b3, b4 = mfj.b4, b5 = mfj.b5, b6 = mfj.b6;
+        taxcalculation(inc, b1, b2, b3, b4, b5, b6);
         break;
     case 3:
         b1 = mfs.b1, b2 = mfs.b2, b3 = mfs.b3, b4 = mfs.b4, b5 = mfs.b5, b6 = mfs.b6;
+        taxcalculation(inc, b1, b2, b3, b4, b5, b6);
         break;
     case 4:
         b1 = hoh.b1, b2 = hoh.b2, b3 = hoh.b3, b4 = hoh.b4, b5 = hoh.b5, b6 = hoh.b6;
+        taxcalculation(inc, b1, b2, b3, b4, b5, b6);
         break;
     case 5:
         cout << "Feature not available yet" << endl;
         break;
     }
 
-    void taxcalculation(){
-        if (inc <= sin.b1) {
-            tax = r1 * inc;
-            cout << "Your yearly tax is " << tax << endl;
-            //taxout();// 
-        }
+    
+}
 
-        else if (inc > sin.b1&& inc <= sin.b2) {
-            tax = r1 * (sin.b1 - 0) + r2 * (inc - sin.b1);
-            cout << "Your yearly tax is " << tax << endl;
-        }
+//calculate taxes
+void taxcalculation(double inc, double b1, double b2, double b3, double b4, double b5, double b6) {
+    double tax;
+    double r1 = 0.10, r2 = 0.12, r3 = 0.22, r4 = 0.24, r5 = 0.32, r6 = 0.35, r7 = 0.37;
+    //
+    if (inc <= b1) {
+        tax = r1 * inc;
+        cout << "Your yearly tax is " << tax << endl;
+        //taxout();// 
+    }
 
-        else if (inc > sin.b2&& inc <= sin.b3) {
-            tax = r1 * (sin.b1 - 0) + r2 * (sin.b2 - sin.b1) + r3 * (inc - sin.b2);
-            cout << "Your yearly tax is " << tax << endl;
-        }
+    else if (inc > b1 && inc <= b2) {
+        tax = r1 * (b1 - 0) + r2 * (inc - b1);
+        cout << "Your yearly tax is " << tax << endl;
+    }
 
-        else if (inc > sin.b3&& inc <= sin.b4) {
-            tax = r1 * (sin.b1 - 0) + r2 * (sin.b2 - sin.b1) + r3 * (sin.b3 - sin.b2) + r4 * (inc - sin.b3);
-            cout << "Your yearly tax is " << tax << endl;
-        }
+    else if (inc > b2 && inc <= b3) {
+        tax = r1 * (b1 - 0) + r2 * (b2 - b1) + r3 * (inc - b2);
+        cout << "Your yearly tax is " << tax << endl;
+    }
 
-        else if (inc > sin.b4&& inc <= sin.b5) {
-            tax = r1 * (sin.b1 - 0) + r2 * (sin.b2 - sin.b1) + r3 * (sin.b3 - sin.b2) + r4 * (sin.b4 - sin.b3) + r5 * (inc - sin.b4);
-            cout << "Your yearly tax is " << tax << endl;
-        }
+    else if (inc > b3 && inc <= b4) {
+        tax = r1 * (b1 - 0) + r2 * (b2 - b1) + r3 * ( - b2) + r4 * (inc - b3);
+        cout << "Your yearly tax is " << tax << endl;
+    }
 
-        else if (inc > sin.b5&& inc <= sin.b6) {
-            tax = r1 * (sin.b1 - 0) + r2 * (sin.b2 - sin.b1) + r3 * (sin.b3 - sin.b2) + r4 * (sin.b4 - sin.b3) + r5 * (sin.b5 - sin.b4) + r6 * (inc - sin.b5);
-            cout << "Your yearly tax is " << tax << endl;
-        }
-        else if (inc > sin.b6) {
-            tax = r1 * (sin.b1 - 0) + r2 * (sin.b2 - sin.b1) + r3 * (sin.b3 - sin.b2) + r4 * (sin.b4 - sin.b3) + r5 * (sin.b5 - sin.b4) + r6 * (sin.b6 - sin.b5) + r7 * (inc - sin.b6);
-            cout << "Your yearly tax is " << tax << endl;
-        }
+    else if (inc > b4 && inc <= b5) {
+        tax = r1 * (b1 - 0) + r2 * (b2 - b1) + r3 * (b3 - b2) + r4 * (b4 - b3) + r5 * (inc - b4);
+        cout << "Your yearly tax is " << tax << endl;
+    }
+
+    else if (inc > b5 && inc <= b6) {
+        tax = r1 * (b1 - 0) + r2 * (b2 - b1) + r3 * (b3 - b2) + r4 * (b4 - b3) + r5 * (inc - b4) + r6 * (inc - b5);
+        cout << "Your yearly tax is " << tax << endl;
+    }
+    else if (inc > b6) {
+        tax = r1 * (b1 - 0) + r2 * (b2 - b1) + r3 * (b3 - b2) + r4 * (b4 - b3) + r5 * (inc - b4) + r6 * (inc - b5) + r7 * (inc - b6);
+        cout << "Your yearly tax is " << tax << endl; 
     }
 }
